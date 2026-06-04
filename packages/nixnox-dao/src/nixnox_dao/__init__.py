d4f10f6bca7d184eff4075394b8cbb216fa5df32
Rafael Gonzalez <astrorafael@yahoo.es>
2026-06-04 11:38:17 +0200

    added comments

--- packages/nixnox-dao/src/nixnox_dao/__init__.py
+++ packages/nixnox-dao/src/nixnox_dao/__init__.py
@@ -1,9 +1,17 @@
+# ------------------
+# Dynamic versioning
+# ------------------
+
 try:
     from importlib.metadata import version, PackageNotFoundError
 
     __version__ = version(__name__.split(".")[-1])
 except (ImportError, PackageNotFoundError, LookupError):
     __version__ = "0.0.0+dev"
+
+# ---------
+# Reimports
+# ---------
 
 from .constants import (
     ObserverType,
